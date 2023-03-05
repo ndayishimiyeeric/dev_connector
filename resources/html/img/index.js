006130bbf5de855c531eaf940180efffdc12dbfb
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-03-05 19:00:57 +0100

    add images

---
+++ resources/html/img/index.js
@@ -0,0 +1,11 @@
+import quotes from "./quotes.svg";
+import people01 from "./people01.jpg";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+
+export {
+  quotes,
+  people01,
+  people02,
+  people03,
+};
