ff9c2519423975c3f83beb300a64642beacaef12
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-03-07 16:42:03 +0100

    refactor

--- profil/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,redirect
-
-from django.contrib.auth.decorators import login_required 
-
-# Create your views here.
-
-@login_required
-def my_profil(request):
-    return render(request,"my_profil.html",context={})
