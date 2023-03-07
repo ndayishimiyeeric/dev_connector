436836e6c422a8757a939dd8e6bcabc9f971ccf4
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-03-07 16:37:36 +0100

    Create Project, Review, Tag models

--- projects/models.py
+++ projects/models.py
@@ -1,16 +1,43 @@
 from django.db import models
 import uuid
-from authentication.models import User
 
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length=200,null=True)
-    description = models.TextField(blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    project_image = models.ImageField(null=True, blank=True, default="defaultProject.jpg" )
     source_code = models.URLField(null=True, blank=True, max_length=2000)
     live_preview = models.URLField(null=True, blank=True, max_length=2000)
-    image=models.ImageField(upload_to="photos/projects/",null=True)
-    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    votes_count = models.IntegerField(default=0, blank=True, null=True)
+    votes_ratio = models.IntegerField(default=0, blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    REVIEW_CHOICES = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    # user
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True, max_length=200)
+    value = models.CharField(choices=REVIEW_CHOICES, max_length=10)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.value
     
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
