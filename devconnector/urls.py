af3252b070464aa8a0c1db870661b8d8766970be
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-03-07 16:30:05 +0100

    refactor static files and urls

--- devconnector/urls.py
+++ devconnector/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from authentication import views
-from profil import views as pv
+
+# for serving media files in development
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("projects.urls")),
-    path("login",views.login_page,name="authentication"),
-    path("register",views.register,name="register"),
-    path('logout',views.logout_user,name="logout"),
-    path("education/",include("education.urls")),
-    path('my_profile',pv.my_profil,name="my_profil"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
