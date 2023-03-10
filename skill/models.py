6f493524d19a7197d43e001f02397096b7cc84f3
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-03-10 14:37:28 +0100

    clean code apply DRY PP8

--- skill/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from authentication.models import User
-
-# Create your models here.
-
-class Skill(models.Model):
-    name=models.CharField(max_length=100)
-    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
-
