c8a6273337bc729c135415cdad4e9a11bd4dbfd2
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-03-12 01:19:52 +0100

    full project CRUD operations

--- projects/views.py
+++ projects/views.py
@@ -26,11 +26,14 @@ def project(request, pk):
 
 @login_required(login_url="login")
 def createProject(request):
+    profile = request.user.profile
     form = ProjectForm()
     if request.method == "POST":
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
             return redirect("projects")
 
     context = {
@@ -41,7 +44,8 @@ def createProject(request):
 
 @login_required(login_url="login")
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
     if request.method == "POST":
@@ -58,7 +62,8 @@ def updateProject(request, pk):
 
 @login_required(login_url="login")
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     if request.method == "POST":
         project.delete()
         return redirect("projects")
