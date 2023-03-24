bedec4c1bdb7b70acc9041e3fda22909e47a4542
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-03-24 16:15:53 +0100

    sort projects by date

--- projects/models.py
+++ projects/models.py
@@ -21,6 +21,9 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['created_at']
+
 
 class Review(models.Model):
     REVIEW_CHOICES = (
