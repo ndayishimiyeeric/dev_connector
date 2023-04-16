8dad97e894f336add25124f18cb551b647e46641
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-04-16 02:08:06 +0200

    render project form

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Project, Review, Tag
+from constants.utils import input_classes, textarea_classes, checkbox_classes
 
 
 class ProjectForm(ModelForm):
@@ -15,6 +16,19 @@ class ProjectForm(ModelForm):
             'live_preview': forms.TextInput(attrs={'placeholder': 'Live Preview'}),
             'tags': forms.CheckboxSelectMultiple(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            if name != 'project_image' or name != 'tags' or name != 'description':
+                field.widget.attrs.update({'class': input_classes})
+            if name == 'project_image':
+                field.widget.attrs.update({'accept': 'image/*'})
+                field.widget.attrs.update({'class': 'sr-only'})
+            if name == 'description':
+                field.widget.attrs.update({'class': textarea_classes})
+            if name == 'tags':
+                field.widget.attrs.update({'class': checkbox_classes, 'type': 'checkbox'})
 
 
 class ReviewForm(ModelForm):
