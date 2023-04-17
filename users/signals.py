df381fbe83661160806bafb2ca258a93c3190566
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-04-17 02:54:14 +0200

    add online signal

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save, post_delete
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
@@ -29,6 +30,18 @@ def updateUser(sender, instance, created, **kwargs):
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
     user.delete()
+
+
+@receiver(user_logged_in)
+def got_online(sender, user, request, **kwargs):
+    user.profile.is_online = True
+    user.profile.save()
+
+
+@receiver(user_logged_out)
+def got_offline(sender, user, request, **kwargs):
+    user.profile.is_online = False
+    user.profile.save()
 
 
 post_save.connect(createProfile, sender=User)
