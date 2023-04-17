8d819b8101631816657b30c397107fa06955bf6d
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-04-17 02:58:17 +0200

    add custom filters

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Skill, Experience, Education
+from .models import Profile, Skill, Experience, Education, Message, Follower
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Skill)
 admin.site.register(Experience)
 admin.site.register(Education)
+admin.site.register(Message)
+admin.site.register(Follower)
