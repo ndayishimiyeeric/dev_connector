b2e837120632deebba9e9f70bd5ab9276601c337
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-05-13 22:28:12 +0200

    fix the import

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-Kofrom django.db import models
+from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.core.exceptions import ValidationError
