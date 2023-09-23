6422fb8bd3c8dd6de663d800ca96c004e63825be
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-09-23 17:54:02 +0200

    customize token creation, refresh, and verification to use cookies

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.get_routes, name="routes"),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('auth/jwt/create/', views.CustomTokenObtainPairView.as_view()),
+    path('auth/jwt/refresh/', views.CustomTokenRefreshView.as_view()),
+    path('auth/jwt/verify/', views.CustomTokenVerifyView.as_view()),
+    path('auth/logout/', views.LogoutView.as_view()),
     path('projects/', views.get_products, name="projects"),
     path('projects/<str:pk>/', views.get_project, name="projects"),
 ]
