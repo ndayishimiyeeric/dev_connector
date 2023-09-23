8789cf0d1b9f72a2466967cd02b50fd05239b247
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-09-23 03:16:17 +0200

    add serializer and projects get views and routes

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
+    path('', views.get_routes, name="routes"),
+    path('projects/', views.get_products, name="projects"),
+    path('projects/<str:pk>/', views.get_project, name="projects"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from api.serializers import ProjectSerializer
+from projects.models import Project
 
 
 @api_view(['GET'])
-def getRoutes(request):
+def get_routes(request):
     routes = [
         {'GET': '/api/products'},
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def get_products(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_project(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
