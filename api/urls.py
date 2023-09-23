6bb1a97402c96c55a6b0b47cefe32d46fbc1300d
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-09-24 01:49:48 +0200

    add urls for social login

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
     path('', views.get_routes, name="routes"),
     path('auth/', include('djoser.urls')),
+    re_path(
+        r'^o/(?P<provider>\S+)/$', views.CustomProviderAuthView.as_view()
+    ),
     path('auth/jwt/create/', views.CustomTokenObtainPairView.as_view()),
     path('auth/jwt/refresh/', views.CustomTokenRefreshView.as_view()),
     path('auth/jwt/verify/', views.CustomTokenVerifyView.as_view()),
